0797ed6252db66ccefdba482e48a4314b0466a40
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-08-29 16:53:51 -0400

    completed question 3

--- linkedList.js
+++ linkedList.js
@@ -26,8 +26,8 @@ class LinkedList {
     // Keep track of previous
     let previousNode = this.head;
 
-    while ((currNode !== null) && (currNode.value !== targetItem)) {
-      // Save the previous node 
+    while (currNode !== null && currNode.value !== targetItem) {
+      // Save the previous node
       previousNode = currNode;
       currNode = currNode.next;
     }
@@ -38,11 +38,10 @@ class LinkedList {
 
     let tempNode = previousNode;
     tempNode.next = new _Node(item, previousNode.next);
-
   }
 
   insertAfter(targetItem, item) {
-    // If the list is empty   
+    // If the list is empty
     if (this.head === null) {
       this.insertFirst(item);
     }
@@ -50,8 +49,8 @@ class LinkedList {
     let currNode = this.head;
     // Keep track of previous
     let previousNode = this.head;
-    while ((currNode !== null) && (currNode.value !== targetItem)) {
-      // Save the previous node 
+    while (currNode !== null && currNode.value !== targetItem) {
+      // Save the previous node
       previousNode = currNode;
       currNode = currNode.next;
     }
@@ -71,27 +70,21 @@ class LinkedList {
       return;
     }
     for (let i = 0; i < position - 1; i++) {
-      if (currNode !== null) {     
+      if (currNode !== null) {
         currNode = currNode.next;
-      } 
-      else {
-        console.log("Invalid position");
+      } else {
+        console.log('Invalid position');
         return;
       }
     }
-    let tempNode = currNode;    
+    let tempNode = currNode;
     tempNode.next = new _Node(item, currNode.next);
-
-
   }
-
-
 
   insertLast(item) {
     if (this.head === null) {
       this.insertFirst(item);
-    }
-    else {
+    } else {
       let tempNode = this.head;
       while (tempNode.next !== null) {
         tempNode = tempNode.next;
@@ -106,15 +99,14 @@ class LinkedList {
     if (!this.head) {
       return null;
     }
-    // Check for the item 
+    // Check for the item
     while (currNode.value !== item) {
       /* Return null if it's the end of the list 
          and the item is not on the list */
       if (currNode.next === null) {
         return null;
-      }
-      else {
-        // Otherwise, keep looking 
+      } else {
+        // Otherwise, keep looking
         currNode = currNode.next;
       }
     }
@@ -137,8 +129,8 @@ class LinkedList {
     // Keep track of previous
     let previousNode = this.head;
 
-    while ((currNode !== null) && (currNode.value !== item)) {
-      // Save the previous node 
+    while (currNode !== null && currNode.value !== item) {
+      // Save the previous node
       previousNode = currNode;
       currNode = currNode.next;
     }
@@ -148,12 +140,11 @@ class LinkedList {
     }
     previousNode.next = currNode.next;
   }
-
 }
 
+let SLL = new LinkedList();
 
-function main() {
-  let SLL = new LinkedList();
+function main(SLL) {
   SLL.insertFirst('Apollo');
   SLL.insertLast('Boomer');
   SLL.insertLast('Helo');
@@ -167,13 +158,70 @@ function main() {
   //Squirrel does not exist so returns item not found
   // SLL.remove('Squirrel');
   //index too large
-  SLL.insertAt('Test', 1);
+  SLL.insertAt('Kat', 3);
+  SLL.remove('Tauhida');
 
-
-
-  console.log(JSON.stringify(SLL));
-
-
+  return SLL;
 }
 
-main();
+function display(SLL) {
+  let currNode = SLL.head;
+  let list = [];
+  while (currNode !== null) {
+    list.push(currNode.value);
+    currNode = currNode.next;
+  }
+  return list;
+}
+
+function size(SLL) {
+  let currNode = SLL.head;
+  let size = 0;
+  while (currNode !== null) {
+    size++;
+    currNode = currNode.next;
+  }
+  return size;
+}
+
+function isEmpty(SLL) {
+  if (!SLL.head) {
+    return 'List is empty';
+  } else {
+    return 'List is not empty';
+  }
+}
+
+function findPreviousNode(SLL, targetItem) {
+  // Start at the head
+  let currNode = SLL.head;
+  // Keep track of previous
+  let previousNode = SLL.head;
+  while (currNode !== null && currNode.value !== targetItem) {
+    // Save the previous node
+    previousNode = currNode;
+    currNode = currNode.next;
+  }
+  if (currNode === null) {
+    console.log('Item not found');
+    return;
+  }
+
+  return previousNode;
+}
+
+function findLastNode(SLL){
+  let tempNode = SLL.head;
+  while (tempNode.next !== null) {
+    tempNode = tempNode.next;
+  }
+
+  return tempNode;
+}
+
+main(SLL);
+console.log(display(SLL));
+console.log(size(SLL));
+console.log(isEmpty(SLL));
+console.log(findPreviousNode(SLL, 'Kat'));
+console.log(findLastNode(SLL));
